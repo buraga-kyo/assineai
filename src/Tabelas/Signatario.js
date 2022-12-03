b309f891b1fb7c8b9b01d95f921fe0905a321a80
Matheus Braga <bragaus@outlook.com>
2022-12-03 19:35:58 -0300

    .

--- src/Tabelas/Signatario.js
+++ src/Tabelas/Signatario.js
@@ -1,5 +1,8 @@
 module.exports = (InstanciaConfiguradaDoSequelize, Sequelize) => {
     const Signatario = InstanciaConfiguradaDoSequelize.define("Signatario", {
+        SignatarioToken: {
+            type: Sequelize.TEXT
+        },
         SignatarioNome: {
             type: Sequelize.STRING,
         },
@@ -9,14 +12,32 @@ module.exports = (InstanciaConfiguradaDoSequelize, Sequelize) => {
         SignatarioModoAutenticacao: {
             type: Sequelize.STRING,
         },
+        SignatarioStatusAssinatura: {
+            type: Sequelize.STRING
+        },
+        SignatarioDataAssinatura: {
+            type: Sequelize.STRING
+        },
+        SignatarioQuantidadeDeAcessosNoLinkDeAssinatura: {
+            type: Sequelize.INTEGER
+        },
+        SignatarioUltimaVisualizacaoNoLinkDeAssinatura: {
+            type: Sequelize.STRING
+        },
+        SignatarioDataAssinatura: {
+            type: Sequelize.STRING
+        },
         SignatarioIp: {
             type: Sequelize.TEXT,
         },
         SignatarioDispositivo: {
             type: Sequelize.TEXT,
         },
-        SignatarioToken: {
-            type: Sequelize.TEXT,
+        SignatarioGeoLatitude: {
+            type: Sequelize.TEXT
+        },
+        SignatarioGeoLongitude: {
+            type: Sequelize.TEXT
         }
     });
 
