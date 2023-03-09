39a1807d557cce02f0b42a4bbf252d951baf8dc6
bragaus <sprtj@protonmail.com>
2023-03-09 20:42:52 -0300

    .

--- src/Utils/Assinatura.js
+++ src/Utils/Assinatura.js
@@ -4,6 +4,41 @@ const FileReader = require('FileReader');
 const fs = require('fs');
 const { sign } = require('pdf-signer-brazil');
 
+
+exports.Assinar = ({ body }, res, ProximaFuncao) => {
+
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'der',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'der',
+        },
+    })
+ 
+    ChavePrivada = privateKey.toString('base64')
+    ChavePublica = publicKey.toString('base64')
+
+    ChavePrivada = crypto.createPrivateKey({
+        key: Buffer.from(ChavePrivada, 'base64'),
+        type: 'pkcs8',
+        format: 'der',
+    })
+    
+    const Hash = crypto.createSign('SHA256')
+    Hash.update(body.Documento)
+    Hash.end()
+    const Assinatura = Hash.sign(ChavePrivada)
+
+	body.ChavePublica = ChavePublica
+	body.Assinatura = Assinatura
+	
+	ProximaFuncao()
+}
+
 exports.AssinarPDFComCertificadoDigital = (req,res) => {
    
     const SenhaDoCertificado = process.env.SENHA_DO_CERTIFICADO
@@ -87,44 +122,6 @@ exports.AssinarPDF = (ArrayUint8, base64ChavePrivada) => {
     return Assinatura.toString('base64')
 }
 
-exports.GerarChavePrivada = (base64ChavePrivada) => {
-
-}
-
-exports.Assinar = ({ body }, res, ProximaFuncao) => {
-
-    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-        modulusLength: 2048,
-        publicKeyEncoding: {
-            type: 'spki',
-            format: 'der',
-        },
-        privateKeyEncoding: {
-            type: 'pkcs8',
-            format: 'der',
-        },
-    })
- 
-    ChavePrivada = privateKey.toString('base64')
-    ChavePublica = publicKey.toString('base64')
-
-    ChavePrivada = crypto.createPrivateKey({
-        key: Buffer.from(ChavePrivada, 'base64'),
-        type: 'pkcs8',
-        format: 'der',
-    })
-    
-    const Hash = crypto.createSign('SHA256')
-    Hash.update(body.Documento)
-    Hash.end()
-    const Assinatura = Hash.sign(ChavePrivada)
-
-	body.ChavePublica = ChavePublica
-	body.Assinatura = Assinatura
-	
-	ProximaFuncao()
-}
-
 exports.VerificarAssinatura = ({ body: {Documento, ChavePublica, Assinatura} }, res) => {
 
     ChavePublica = crypto.createPublicKey({
@@ -142,39 +139,39 @@ exports.VerificarAssinatura = ({ body: {Documento, ChavePublica, Assinatura} }, 
 }
 
 
-    // var form = new formidable.IncomingForm();
-    // form.parse(req, (err, fields, files) => {
+// var form = new formidable.IncomingForm();
+// form.parse(req, (err, fields, files) => {
 
-    //     getAsByteArray(files).then((btes) => {
-    //         console.log(btes)
-    //     })
-    // })
+//     getAsByteArray(files).then((btes) => {
+//         console.log(btes)
+//     })
+// })
 
-    // function readFile(file) {
-    //     return new Promise((resolve, reject) => {
-    //       // Create file reader
-    //       let reader = new FileReader()
-      
-    //       // Register event listeners
-    //       reader.addEventListener("loadend", e => resolve(e.target.result))
-    //       reader.addEventListener("error", reject)
-      
-    //       // Read file
-    //       reader.readAsArrayBuffer(file)
-    //     })
-    //   }
+// function readFile(file) {
+//     return new Promise((resolve, reject) => {
+//       // Create file reader
+//       let reader = new FileReader()
+    
+//       // Register event listeners
+//       reader.addEventListener("loadend", e => resolve(e.target.result))
+//       reader.addEventListener("error", reject)
+    
+//       // Read file
+//       reader.readAsArrayBuffer(file)
+//     })
+//   }
 
-    // async function getAsByteArray(file) {
-    //     return new Uint8Array(await readFile(file))
-    // }
+// async function getAsByteArray(file) {
+//     return new Uint8Array(await readFile(file))
+// }
 
-    // function getByteArray(filePath){
-    //     let fileData = fs.readFileSync(filePath).toString('hex');
-    //     let result = []
-    //     for (var i = 0; i < fileData.length; i+=2)
-    //       result.push('0x'+fileData[i]+''+fileData[i+1])
-    //     return result;
-    // }    
-    // const Hash = crypto.createSign('SHA256')
-    // Hash.update(body.Documento)
-    // Hash.end()
+// function getByteArray(filePath){
+//     let fileData = fs.readFileSync(filePath).toString('hex');
+//     let result = []
+//     for (var i = 0; i < fileData.length; i+=2)
+//       result.push('0x'+fileData[i]+''+fileData[i+1])
+//     return result;
+// }    
+// const Hash = crypto.createSign('SHA256')
+// Hash.update(body.Documento)
+// Hash.end()
