05790781ce3c0c6dfee776c2c54335a8b7ecb01f
bragaus <sprtj@protonmail.com>
2023-03-15 10:01:17 -0300

    .

--- src/Utils/CriarPaginaDeAssinaturaDoPDF.js
+++ src/Utils/CriarPaginaDeAssinaturaDoPDF.js
@@ -1,5 +1,7 @@
 const { PDFDocument } = require("pdf-lib");
-// const { appendFileSync } = require("fs");
+// const base64 = require('base64topdf');
+const fs = require("fs");
+const { writeFileSync } = require("fs");
 
 module.exports = async (DocumentoBase64) =>  {
     
@@ -12,6 +14,30 @@ module.exports = async (DocumentoBase64) =>  {
     const BytesDoPDF = await PDF.save()
     const DocumentoBase64Atualizado = Buffer.from(BytesDoPDF).toString('base64')
 
+    fs.writeFile("exemplo.pdf", BytesDoPDF, function(err) {
+       console.log(err);
+    });    
+
     return DocumentoBase64Atualizado
+
+    //const PDF = await PDFDocument.load(BufferDoBase64)
+
+    //const pagina = PDF.addPage();
+    //pagina.drawText('Exemplo', {x:0,y:820,size:20})
+    //const BytesDoPDF = await PDF.save()
+    //const DocumentoBase64Atualizado = Buffer.from(DocumentoBase64)
+
+    // writeFileSync("blank.pdf", await PDF.save());
+    // var buf = Buffer.from(DocumentoBase64Atualizado, 'base64');
+
+    // let decodedBase64 = base64.base64Decode(DocumentoBase64Atualizado, 'Exemplo.pdf');
+    // console.log(decodedBase64);
+    //https://avepdf.com/convert-to-pdfa
+    //PDF/A 
+    // fs.writeFile("samples.pdf", BufferDoBase64, function(err) {
+    //    console.log(err);
+    // });
+
+    // return ""//DocumentoBase64Atualizado
 
 }
