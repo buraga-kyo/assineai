40f5c05373a67e396a84d5d97cb56c901d9469fe
bragaus <sprtj@protonmail.com>
2023-06-28 19:48:43 -0300

    corpo do layout

--- ModuloRaiz/RegrasDeNegocio/Procedimentos/CriarDocumentoViaAPI.js
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/CriarDocumentoViaAPI.js
@@ -24,7 +24,7 @@ module.exports = async (Requisicao, Resposta, ProximaFuncao) => {
             Signatario.create(RegistroDoSignatario)
         })
         
-        const DocumentoBase64Atualizado = await ConstruirPaginaComDadosDeAssinatura(RegistroArquivo.ArquivoBase64, DocumentoId)
+        const DocumentoBase64Atualizado = await ConstruirPaginaComDadosDeAssinatura(RegistroArquivo.ArquivoBase64, DocumentoId, ColecaoDeSignatarios)
 
         const { ArquivoId: ArquivoEmAndamentoId } = await Arquivo.create({ ArquivoBase64: DocumentoBase64Atualizado })
 
