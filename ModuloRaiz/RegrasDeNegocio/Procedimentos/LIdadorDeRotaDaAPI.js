c2e62a4e3e3a49067ed38b9a80656af9c5bbae5f
bragaus <sprtj@protonmail.com>
2023-07-26 15:37:39 -0300

    diversas mudanças em diversos lugares em diversos momentos

--- ModuloRaiz/RegrasDeNegocio/Procedimentos/LIdadorDeRotaDaAPI.js
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/LIdadorDeRotaDaAPI.js
@@ -3,14 +3,14 @@ const CriarDocumentoViaAPI = require("./CriarDocumentoViaAPI")
 const AdicionarDocumentoExtraViaAPI = require("./AdicionarDocumentoExtraViaAPI")
 const AssinarDocumentoViaAPI = require("./AssinarDocumentoViaAPI")
 const ListarDocumentos = require("./ListarDocumentos")
+const ListarDocumento = require("./ListarDocumento")
 
 rotas.post("/CriarDocumentoViaAPI", CriarDocumentoViaAPI)
 rotas.post("/AdicionarDocumentoExtraViaAPI/:DocumentoToken", AdicionarDocumentoExtraViaAPI)
 rotas.post("/AssinarDocumentoViaAPI", AssinarDocumentoViaAPI)
 
 rotas.post("/ListarDocumentos", ListarDocumentos)
-
-
+rotas.get("/ListarDocumento/:token", ListarDocumento)
 
 const teste = require("./LidadorDeAssinaturaViaAPI_Backup")
 rotas.post("/teste", teste)
---
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/ListarDocumento.js
@@ -0,0 +1,36 @@
+const { Documento, Signatario, Arquivo } = require("../../BancoDeDados/Conector").Tabelas;
+
+module.exports = async (Requisicao, Resposta) => {
+    
+    try {
+
+        let Documentos = await Documento.findAll({
+            where: { DocumentoToken: Requisicao.params.token },
+            attributes: ["DocumentoId","DocumentoNome","DocumentoStatusAssinatura","DocumentoToken","DocumentoResponsavel","createdAt"]
+        })
+        let Signatarios = []
+        
+        for await (let Documento of Documentos){
+            
+            Signatarios = await Signatario.findAll({ 
+                where: { DocumentoId: Documento.dataValues.DocumentoId },
+                attributes: ["SignatarioNome", "SignatarioStatusAssinatura", "SignatarioId"]
+            })
+            
+            TotalDeSignatarios = await Signatario.count({ where: { DocumentoId: Documento.dataValues.DocumentoId } })
+            TotalDeAssinaturas = await Signatario.count({ where: { SignatarioStatusAssinatura: 'Assinado', DocumentoId: Documento.dataValues.DocumentoId } })
+
+            Documentos[Documentos.indexOf(Documento)].dataValues.Signatarios = Signatarios;
+            Documentos[Documentos.indexOf(Documento)].dataValues.TotalDeSignatarios = TotalDeSignatarios;
+            Documentos[Documentos.indexOf(Documento)].dataValues.TotalDeAssinaturas = TotalDeAssinaturas;
+        }
+
+        Resposta.json(Documentos)
+
+    } catch (Erro) {
+        console.log(Erro)
+        Resposta.sendStatus(500)
+    }
+
+    
+}
