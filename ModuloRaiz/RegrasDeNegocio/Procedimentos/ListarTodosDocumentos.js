87d18ffae84c1d77a3db92964122f08e4207a5d6
bragaus <sprtj@protonmail.com>
2023-09-05 17:50:03 -0300

    .

--- ModuloRaiz/RegrasDeNegocio/Procedimentos/ListarTodosDocumentos.js
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/ListarTodosDocumentos.js
@@ -2,60 +2,66 @@ const { Documento, Signatario, Arquivo } = require("../../BancoDeDados/Conector"
 const Op = require("sequelize").Op;
 
 module.exports = async (Requisicao, Resposta) => {
-    
+
     try {
 
-        const { FiltroDeStatus, FiltroDeProcura } = Requisicao.body;
+        const { FiltroDeStatus, FiltroDeProcura, FiltroDeData } = Requisicao.body;
 
-        var where = {};
-        if(FiltroDeStatus != null){
-            where = {
-                [Op.and] : [
-                    {
-                        DocumentoStatusAssinatura:{
-                            [Op.eq] : FiltroDeStatus
-                        } 
-                    },
-                    {
-                        [Op.or]: [
-                            {
-                                DocumentoNome: {
-                                    [Op.iLike] : FiltroDeProcura + "%"
-                                }
-                            },
-                        ],
-                    }
-                ]
-                
-                
-            };
-        }else{
-            where = {
-                [Op.or]: [
-                    {
-                        DocumentoNome: {
-                            [Op.iLike] : FiltroDeProcura + "%"
-                        }
-                    },
-                ] 
-            };
-        }
+        console.log(FiltroDeData)
+
+        const where = {};
+        if (FiltroDeStatus) where.DocumentoStatusAssinatura = FiltroDeStatus;
+        if (FiltroDeProcura) where.DocumentoNome = { [Op.iLike]: "%"+FiltroDeProcura+"%" }
+        if (FiltroDeData?.includes(" to ")) where.createdAt = { [Op.between]: [FiltroDeData.split(" to ")[0], FiltroDeData.split(" to ")[1]] }
+
+        // if(FiltroDeStatus != null){
+        //     where = {
+        //         [Op.and] : [
+        //             {
+        //                 DocumentoStatusAssinatura:{
+        //                     [Op.eq] : FiltroDeStatus
+        //                 } 
+        //             },
+        //             {
+        //                 [Op.or]: [
+        //                     {
+        //                         DocumentoNome: {
+        //                             [Op.iLike] : FiltroDeProcura + "%"
+        //                         }
+        //                     },
+        //                 ],
+        //             }
+        //         ]
+
+
+        //     };
+        // }else{
+        //     where = {
+        //         [Op.or]: [
+        //             {
+        //                 DocumentoNome: {
+        //                     [Op.iLike] : FiltroDeProcura + "%"
+        //                 }
+        //             },
+        //         ] 
+        //     };
+        // }
 
         let Documentos = await Documento.findAndCountAll({
             where: where,
             offset: Requisicao.body.QtdPularRegistrosPular,
             limit: Requisicao.body.limiteRegistros,
-            attributes: ["DocumentoId","DocumentoNome","DocumentoStatusAssinatura","DocumentoToken","createdAt"]
+            attributes: ["DocumentoId", "DocumentoNome", "DocumentoStatusAssinatura", "DocumentoToken", "createdAt"]
         })
         let Signatarios = []
-        
-        for await (let Documento of Documentos.rows){
-            
-            Signatarios = await Signatario.findAll({ 
+
+        for await (let Documento of Documentos.rows) {
+
+            Signatarios = await Signatario.findAll({
                 where: { DocumentoId: Documento.dataValues.DocumentoId },
                 attributes: ["SignatarioNome", "SignatarioStatusAssinatura", "SignatarioId"]
             })
-            
+
             Documentos.rows[Documentos.rows.indexOf(Documento)].dataValues.Signatarios = Signatarios;
 
         }
@@ -72,5 +78,5 @@ module.exports = async (Requisicao, Resposta) => {
         Resposta.sendStatus(500)
     }
 
-    
+
 }
