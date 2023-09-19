bb85ebb8f1c4ba07a7a7d41f0d48eaa4dffe0982
bragaus <sprtj@protonmail.com>
2023-09-19 19:54:13 -0300

    diversas mudanças em diversos estados em diversos momentos em diversos universos em diversos instantes

--- ModuloDoServidor/Iniciar.js
+++ ModuloDoServidor/Iniciar.js
@@ -1,26 +1,23 @@
-require('dotenv').config();
-require("../ModuloRaiz/BancoDeDados/Conector").InstanciaConfiguradaDoSequelize.sync({ force: false }).catch(erro => console.log(erro));
+require('dotenv').config()
 
-const express = require("express");
-const cors = require("cors");
-const morgan = require('morgan');
+require("../ModuloRaiz/BancoDeDados/Conector")
+    .InstanciaConfiguradaDoSequelize
+    .sync({ force: true })
+    .catch(erro => console.log(erro))
 
-const LidadorDeRotasDaAPI = require("../ModuloRaiz/RegrasDeNegocio/Procedimentos/LidadorDeRotaDaAPI");
+const express = require("express")
+const cors = require("cors")
+const morgan = require('morgan')
 
-const Servidor = express();
+const LidadorDeRotasDaAPI = require("../ModuloRaiz/RegrasDeNegocio/Procedimentos/LidadorDeRotaDaAPI")
 
-// Servidor.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//   next()
-// })
+const Servidor = express()
 
-Servidor.use(morgan('dev'));
-Servidor.use(cors()); // { credentials: true, origin: process.env.ORIGIN }
-Servidor.use(express.json({limit: '50mb'}));
-Servidor.use(express.urlencoded({ extended: false}));
-Servidor.use(LidadorDeRotasDaAPI);
-//Servidor.use(cors({credentials: true, origin: process.env.ORIGIN}));
-Servidor.listen(process.env.PORT || 3000);
+Servidor.use(morgan('dev'))
+Servidor.use(cors())
+Servidor.use(express.json({limit: '50mb'}))
+Servidor.use(express.urlencoded({ extended: false}))
+Servidor.use(LidadorDeRotasDaAPI)
+Servidor.listen(process.env.PORT || 3000)
 
 
