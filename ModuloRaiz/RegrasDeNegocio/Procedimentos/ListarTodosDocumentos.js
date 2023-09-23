bd2446fa03da0bf70e038c8f935ff6b018e5c085
bragaus <sprtj@protonmail.com>
2023-09-23 08:41:51 -0300

    diversar diversos em diversos diversas em diversas diversos

--- ModuloRaiz/RegrasDeNegocio/Procedimentos/ListarTodosDocumentos.js
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/ListarTodosDocumentos.js
@@ -7,16 +7,56 @@ module.exports = async (Requisicao, Resposta) => {
     try {
 
         const { FiltroDeStatus, FiltroDeProcura, FiltroDeData, Sort } = Requisicao.body;
-        console.log(FiltroDeData)
         const Filtro = {};
+
         if (FiltroDeStatus) Filtro.DocumentoStatusAssinatura = FiltroDeStatus;
         if (FiltroDeProcura) Filtro.DocumentoNome = { [Op.iLike]: "%"+FiltroDeProcura+"%" }
-        if (FiltroDeData?.includes(" até ")) Filtro.DocumentoDataDeGeracao = { [Op.between]: [FiltroDeData.split(" até ")[0], FiltroDeData.split(" até ")[1]] }
-        else if (FiltroDeData != null) {  
-                Filtro.DocumentoDataDeGeracao =  InstanciaConfiguradaDoSequelize.where(
-                    InstanciaConfiguradaDoSequelize.fn('date_trunc', 'day', InstanciaConfiguradaDoSequelize.col('DocumentoDataDeGeracao')),
-                FiltroDeData
-            )
+        if (FiltroDeData != null) {
+
+            if (FiltroDeData?.includes(" até ")) {
+
+                const DataAntesDoAte = FiltroDeData.split(" até ")[0]
+
+                var PartesDaData = DataAntesDoAte.split('-')
+                var dia = PartesDaData[0]
+                var mes = PartesDaData[1]
+                var ano = PartesDaData[2]
+
+                var DataAntesDoAteFormatada = `${ano}-${mes}-${dia}`
+
+                const DataDepoisDoAte = FiltroDeData.split(" até ")[1]
+
+                PartesDaData = DataDepoisDoAte.split('-')
+                dia = PartesDaData[0]
+                mes = PartesDaData[1]
+                ano = PartesDaData[2]
+
+                var DataDepoisDoAteFormatada = `${ano}-${mes}-${dia}`
+                
+                var DataInicial = new Date(DataAntesDoAteFormatada)
+                var DataFinal = new Date(DataDepoisDoAteFormatada)
+
+                DataFinal.setUTCHours(23,59,59,999); //Setando a hora da data fim no final do dia
+
+                Filtro.DocumentoDataDeGeracao = { [Op.between]: [DataInicial, DataFinal] }
+
+            } else {
+
+                const PartesDaData = FiltroDeData.split('-')
+                const dia = PartesDaData[0]
+                const mes = PartesDaData[1]
+                const ano = PartesDaData[2]
+                var DataFormatada = `${ano}-${mes}-${dia}`
+
+                var DataInicial = new Date(DataFormatada)
+                var DataFinal = new Date(DataFormatada)
+
+                DataFinal.setUTCHours(23,59,59,999)
+
+                Filtro.DocumentoDataDeGeracao = { [Op.between]: [DataInicial, DataFinal] }
+
+            }
+
         }
 
         let Order = [];
