fabaaada94b4d105da5ca95427c5c46bfd5bbbec
bragaus <sprtj@protonmail.com>
2025-01-31 17:28:45 -0300

    add assineai construir pdf

--- ModuloRaiz/RegrasDeNegocio/Procedimentos/AssineAi_AssinarDocumentoViaAPI.js
+++ ModuloRaiz/RegrasDeNegocio/Procedimentos/AssineAi_AssinarDocumentoViaAPI.js
@@ -1,27 +1,49 @@
-const GCA_ConstruirPaginaComDadosDeAssinatura = require("../Ferramentas/ManipulacaoDePDF/GCA_ConstruirPaginaComDadosDeAssinatura")
+const { Assinatura, Signatarios, Documentos } = require("../../BancoDeDados/Conector").Tabelas;
+const AssineAi_ConstruirPaginaComDadosDeAssinatura = require("../Ferramentas/ManipulacaoDePDF/AssineAi_ConstruirPaginaComDadosDeAssinatura")
 const CriarArquivoPDFApartirDoBase64 = require("../Ferramentas/ManipulacaoDePDF/CriarArquivoPDFApartirDoBase64")
 const AssinarPDFcomCertificadoDigital = require("../Ferramentas/LidarComAssinatura/CertificadoDigital")
 const AssinarPDFcomCriptografiaAssimetrica = require("../Ferramentas/LidarComAssinatura/CriptografiaAssimetrica")
-const fs = require("fs")
+const fs = require("fs");
+const { raw } = require("body-parser");
 
-module.exports = async (Req, Resposta) => {
+module.exports = async (Requisicao, Resposta) => {
 
-    try {     
+    try {
 
-        console.log(Req.body);
+        const signatario = await Signatarios.findOne({
+            raw: true,
+            where: {
+                SignatarioTokenLinkAssinatura: Requisicao.body.SignatarioToken
+            },
+            attributes: ["AssinaturaId"]
+        });
 
-        /*const ColecaoDeDocumentos = []
+        const signatarios = await Signatarios.findAll({
+            raw: true,
+            where: {
+                AssinaturaId: signatario.AssinaturaId
+            }
+        });
+
+        const documentos = await Documentos.findAll({
+            raw: true,
+            where: {
+                AssinaturaId: signatario.AssinaturaId
+            }
+        });
+
+        const ColecaoDeDocumentos = []
         var DocumentoBase64Atualizado = ""
         var NomeDoArquivo = ""
         var CaminhoDoArquivo = ""
         var ArquivoPDFCriadoComSucesso = false
 
-        for await (const documento of Documentos) {
+        for await (const documento of documentos) {
 
-            NomeDoArquivo = documento.DocumentoToken+".pdf"
-            DocumentoBase64Atualizado = await GCA_ConstruirPaginaComDadosDeAssinatura(documento, Signatarios, GravarSelfie)
-            CaminhoDoArquivo = process.env.BaseDir+"/Arquivos/Temporario/"+NomeDoArquivo
-            ArquivoPDFCriadoComSucesso = await CriarArquivoPDFApartirDoBase64(CaminhoDoArquivo, DocumentoBase64Atualizado)
+            NomeDoArquivo = documento.DocumentoHashDoPDFOriginal + ".pdf"
+            DocumentoBase64Atualizado = await AssineAi_ConstruirPaginaComDadosDeAssinatura(documento, signatarios, false)
+            CaminhoDoArquivo = process.env.BaseDir + "/Arquivos/Temporario/" + NomeDoArquivo
+            /*ArquivoPDFCriadoComSucesso = await CriarArquivoPDFApartirDoBase64(CaminhoDoArquivo, DocumentoBase64Atualizado)
 
             if (ArquivoPDFCriadoComSucesso) {
         
@@ -46,13 +68,13 @@ module.exports = async (Req, Resposta) => {
                 DocumentoBase64Atualizado: Base64PDFComCertificado,
                 ChavePublica: DadosCriptografiaAssimetrica.ChavePublica,
                 Assinatura: DadosCriptografiaAssimetrica.Assinatura
-            })
+            })*/
         }
 
-        Resposta.status(200).json(ColecaoDeDocumentos)*/
+        Resposta.status(200).json(ColecaoDeDocumentos)
 
     } catch (error) {
-            
+
         console.error('-----------------------------------------------------------------------------------')
         console.error('***********************************************************************************')
         console.error(error)
