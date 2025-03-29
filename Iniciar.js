f81966a9bb09596452aac200ce4e9424cb77a605
bragaus <sprtj@protonmail.com>
2025-03-29 19:15:36 -0300

    refactor

--- Iniciar.js
+++ Iniciar.js
@@ -1,5 +1,5 @@
-require('dotenv').config({ path: __dirname + '/.env' })
-require("../ModuloRaiz/BancoDeDados/Conector")
+require('dotenv')
+require("./ModuloRaiz/BancoDeDados/Conector")
     .InstanciaConfiguradaDoSequelize
     .sync({ alter: true })
     .catch(erro => console.log(erro))
@@ -7,14 +7,19 @@ require("../ModuloRaiz/BancoDeDados/Conector")
 const express = require("express")
 const cors = require("cors")
 const morgan = require('morgan')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 
-process.env.BaseDir = __dirname;
-//process.env.SENHA_DO_CERTIFICADO=25397272;
-
-const LidarComRotasDaAPI = require("../ModuloRaiz/RegrasDeNegocio/Procedimentos/LidarComRotasDaAPI")
+const LidarComRotasDaAPI = require("./ModuloRaiz/RegrasDeNegocio/Procedimentos/LidarComRotasDaAPI")
 
 const Servidor = express()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100 // limite de 100 requisições por IP
+})
+Servidor.use(limiter)
+Servidor.use(helmet())
 Servidor.use(morgan('dev'))
 Servidor.use(cors())
 Servidor.use(express.json({ limit: '50mb' }))
