00bfb037a0394c2a5d6f80c1a2da889a7ecf349d
Braga Us <sprtj@protonmail.com>
2025-06-13 16:35:38 -0300

    docs: atualiza comentario da funcao DataAtualFormatada

--- ModuloRaiz/RegrasDeNegocio/Ferramentas/FuncoesGenericas/DataAtualFormatada.js
+++ ModuloRaiz/RegrasDeNegocio/Ferramentas/FuncoesGenericas/DataAtualFormatada.js
@@ -2,7 +2,7 @@ module.exports = () => {
     var data = new Date(),
     dia  = data.getDate().toString(),
     diaF = (dia.length == 1) ? '0'+dia : dia,
-    mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+    mes  = (data.getMonth() + 1).toString(), // +1 porque janeiro é representado por 0
     mesF = (mes.length == 1) ? '0'+mes : mes,
     anoF = data.getFullYear();
     Hora = data.getHours().toString()
