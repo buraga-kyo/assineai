bb84117be146a4ce342543553661e39c44340f55
Braga Us <sprtj@protonmail.com>
2025-06-13 16:10:45 -0300

    Fix Sequelize boolean type in migration

--- migrations/20241101210830-add_new_column_to_table.js
+++ migrations/20241101210830-add_new_column_to_table.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('AssinaturaSignatarios', 'SignatarioAssinou', {
-      type: Sequelize.BOOlEAN,
+      type: Sequelize.BOOLEAN,
       allowNull: true,
       defaultValue: false
     });
